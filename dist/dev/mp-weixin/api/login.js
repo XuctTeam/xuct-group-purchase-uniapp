91aef568a7c4695c4122e2878771778fddfecce4
徐涛 <jianhao2010303@163.com>
2023-03-22 15:43:20 +0800

    Delete dist/dev/mp-weixin directory

--- dist/dev/mp-weixin/api/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const services_request = require("../services/request.js");
-const loginByCode = (code) => {
-  return services_request.instance.post("/login/code", {
-    code
-  });
-};
-exports.loginByCode = loginByCode;
--- dist/dev/mp-weixin/constant/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var StoreEnum = /* @__PURE__ */ ((StoreEnum2) => {
-  StoreEnum2["TOKEN"] = "asscee_token";
-  StoreEnum2["USER"] = "user_info";
-  return StoreEnum2;
-})(StoreEnum || {});
-exports.StoreEnum = StoreEnum;
